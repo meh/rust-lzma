3d5eb4a844d294998b9ef3ed2e60fbfe37036f76
meh <meh@schizofreni.co>
2015-07-25 08:01:58 +0200

    reader/length: some tyle fixes

--- src/reader/length.rs
+++ src/reader/length.rs
@@ -6,8 +6,7 @@ use super::{BitTree, Range};
 
 #[derive(Clone, Debug)]
 pub struct Length {
-	choice:  u16,
-	choice2: u16,
+	choice: [u16; 2],
 
 	low: Vec<BitTree>,
 	mid: Vec<BitTree>,
@@ -17,8 +16,7 @@ pub struct Length {
 impl Length {
 	pub fn new() -> Self {
 		Length {
-			choice:  PROBABILITY_INITIAL_VALUE,
-			choice2: PROBABILITY_INITIAL_VALUE,
+			choice: [PROBABILITY_INITIAL_VALUE; 2],
 
 			low: vec![BitTree::new(3); 1 << POSITION_BITS_MAX],
 			mid: vec![BitTree::new(3); 1 << POSITION_BITS_MAX],
@@ -27,10 +25,10 @@ impl Length {
 	}
 
 	pub fn decode<T: Read>(&mut self, mut stream: T, range: &mut Range, state: usize) -> Result<usize, Error> {
-		if !try!(range.probabilistic(stream.by_ref(), &mut self.choice)) {
+		if !try!(range.probabilistic(stream.by_ref(), &mut self.choice[0])) {
 			Ok(try!(self.low[state].decode(stream.by_ref(), range)))
 		}
-		else if !try!(range.probabilistic(stream.by_ref(), &mut self.choice2)) {
+		else if !try!(range.probabilistic(stream.by_ref(), &mut self.choice[1])) {
 			Ok(8 + try!(self.mid[state].decode(stream.by_ref(), range)))
 		}
 		else {
